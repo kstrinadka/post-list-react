b26850eb41f071b50f72286b2a872e69e8be8171
kstrinadka <k.strinadka@gmail.com>
2023-06-06 16:51:56 +0700

    - Еще один способ добавление стилей для компонентов (стиль как свойство объекта) - Добавлены поля ввода и кнопки для создания нового поста + стили к ним

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import "../src/styles/App.css"
 import {useState} from "react";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
 
@@ -11,19 +13,20 @@ function App() {
         {id: 3, title: 'Javascript 3', body: 'Description 3'}
     ]);
 
-    const [posts2, setPosts2] = useState([
-        {id: 1, title: 'Python', body: 'Description'},
-        {id: 2, title: 'Python 2', body: 'Description 2'},
-        {id: 3, title: 'Python 3', body: 'Description 3'}
-    ]);
-
 
 
     return (
         <div className="App">
 
+            <form>
+                <MyInput type="text" placeholder="Навание поста"/>
+                <MyInput type="text" placeholder="Описание поста"/>
+            </form>
+
+            <MyButton disabled>Создать пост</MyButton>
+
             <PostList posts={posts} title={"Посты про JS"}/>
-            <PostList posts={posts2} title={"Посты про Python"}/>
+
 
         </div>
 

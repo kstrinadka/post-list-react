30c43e0774b989c651750b742bc085b1c9ac2f89
kstrinadka <k.strinadka@gmail.com>
2023-06-06 16:15:22 +0700

    - Добавлен список постов PostList.jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 
 import "../src/styles/App.css"
-import PostItem from "./components/PostItem";
+import {useState} from "react";
+import PostList from "./components/PostList";
 
 function App() {
+
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript 2', body: 'Description 2'},
+        {id: 3, title: 'Javascript 3', body: 'Description 3'}
+    ]);
+
 
 
     return (
         <div className="App">
-            
-            <PostItem post={{id: 1, title: 'Javascript', body: 'Description'}}/>
+
+            <PostList posts={posts} title={"Список постов 1"}/>
+            <PostList posts={posts} title={"Список постов 2"}/>
 
         </div>
 

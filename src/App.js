c13661c141e7d59cbb550199e4a8f0c34500cb19
kstrinadka <k.strinadka@gmail.com>
2023-06-06 15:10:09 +0700

    Заголовок изменяется при изменении текстового поля

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // состояние состоит из 2-х элементов: значение и функция для изменения значения
+    const [number, setNumber] = useState(0);
+
+    const [value, setValue] = useState(null);
+
+    function onClickIncrement() {
+        setNumber(number + 1)
+        console.log("ТЫк!")
+    }
+
+    function onClickDecrement() {
+        setNumber(number - 1)
+        console.log("ТЫк!")
+    }
+
+    function onClickSetText(newValue) {
+        setValue(newValue)
+        console.log("new text in h1")
+    }
+
+
+    return (
+        <div className="App">
+            Приложение работает!
+            <h1>
+                {number}
+            </h1>
+            <button className="but" onClick={onClickIncrement}>
+                +1
+            </button>
+            <button className="but" onClick={onClickDecrement}>
+                -1
+            </button>
+
+            <div>
+                <h1>{value}</h1>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={event => setValue(event.target.value)}
+                />
+                <button className="textBut" onClick={() => onClickSetText(value)}>
+                    Изменить заголовок с помощью кнопки
+                </button>
+            </div>
+        </div>
+
+
+
+    );
 }
 
 export default App;

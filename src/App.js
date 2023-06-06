04e58a32cc6c30f879f91ec5b4adbeb72dcb9344
kstrinadka <k.strinadka@gmail.com>
2023-06-06 16:16:56 +0700

    - Добавлен список еще один список постов для наглядности

--- src/App.js
+++ src/App.js
@@ -11,13 +11,19 @@ function App() {
         {id: 3, title: 'Javascript 3', body: 'Description 3'}
     ]);
 
+    const [posts2, setPosts2] = useState([
+        {id: 1, title: 'Python', body: 'Description'},
+        {id: 2, title: 'Python 2', body: 'Description 2'},
+        {id: 3, title: 'Python 3', body: 'Description 3'}
+    ]);
+
 
 
     return (
         <div className="App">
 
-            <PostList posts={posts} title={"Список постов 1"}/>
-            <PostList posts={posts} title={"Список постов 2"}/>
+            <PostList posts={posts} title={"Посты про JS"}/>
+            <PostList posts={posts2} title={"Посты про Python"}/>
 
         </div>
 

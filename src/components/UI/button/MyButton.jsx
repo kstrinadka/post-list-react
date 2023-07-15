ca9c16d182e9f4f24f11cdc8e44236174f90d70d
kstrinadka <k.strinadka@gmail.com>
2023-07-15 17:59:03 +0700

    some changes and comments

--- src/components/UI/button/MyButton.jsx
+++ src/components/UI/button/MyButton.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import classes from "./MyButton.module.css";
 
+/**
+ * className={classes.myBtn} -> получаем стиль как свойство объекта
+ */
 const MyButton = ({children, ...props}) => {
     return (
         <button {...props} className={classes.myBtn}>
--- src/components/UI/input/MyInput.jsx
+++ src/components/UI/input/MyInput.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import classes from './MyInput.module.css'
 
+/**
+ * React.forwardRef(). Эта функция позволяет передавать ref (ссылку на DOM-элемент) внутрь компонента.
+ * ref является ссылкой на DOM-элемент, который будет присвоен данному компоненту.
+ */
 const MyInput = React.forwardRef((props, ref) => {
     return (
         <input ref={ref} className={classes.myInput} {...props}/>
